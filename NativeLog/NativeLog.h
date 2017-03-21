226370d5394aabb9a0ec726e255dd3f21c130d7c
naivehhr <1903283729@qq.com>
2017-03-21 11:11:44 +0800

    Initial Commit

---
+++ NativeLog/NativeLog.h
@@ -0,0 +1,13 @@
+//
+//  NativeLog.h
+//  NativeLog
+//
+//  Created by aran.hu on 17/3/21.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NativeLog : NSObject
+
+@end
---
+++ NativeLog/NativeLog.m
@@ -0,0 +1,13 @@
+//
+//  NativeLog.m
+//  NativeLog
+//
+//  Created by aran.hu on 17/3/21.
+//
+//
+
+#import "NativeLog.h"
+
+@implementation NativeLog
+
+@end
